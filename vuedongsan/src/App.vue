75ee5766f7b753aea50c92d53e5861cfb7463f4a
DESKTOP-O9VECB5\admin <dud5825@naver.com>
2025-01-14 13:02:23 +0900

    [조영민] 2025-01-14 -6강 v-if 모달창 만들기 숙제까지 완료

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,4 +1,11 @@
 <template>
+    <div class="black-bg" v-if="modalOne == true">
+        <div class="white-bg">
+            <h4>상세페이지임</h4>
+            <p>상세페이지 내용임</p>
+            <h2 @click="modalOne = !modalOne">닫기</h2>
+        </div>
+    </div>
 
     <div class="menu">
         <!-- 
@@ -18,24 +25,30 @@
             <h4 class="red">{{products[i]}}</h4>
             <p>50만원</p>
         </div> -->
+        <!-- 
+            @click or v-on:click = "JavaScript"  
+        -->
+        <!-- <button @click="신고수 += 1">허위매물 신고</button> <span>신고수 : {{신고수}}</span> -->
         <div>
-            <h4>{{products[0]}}</h4>
+            <img src="./assets/room0.jpg" class="room-img">
+            <h4 @click="modalOne = true">{{products[0]}}</h4>
             <p>50만원</p>
-            <!-- 
-                @click or v-on:click = "JavaScript"  
-            -->
-            <!-- <button @click="신고수 += 1">허위매물 신고</button> <span>신고수 : {{신고수}}</span> -->
-            <button @click="신고수[0]++">허위매물 신고</button> <span>신고수 : {{신고수[0]}}</span>
+            <button @click="신고수[0]++">허위매물 신고</button>
+            <span>신고수 : {{신고수[0]}}</span>
         </div>
         <div>
+            <img src="./assets/room1.jpg" class="room-img">
             <h4>{{products[1]}}</h4>
             <p>60만원</p>
-            <button @click="신고수[1]++">허위매물 신고</button> <span>신고수 : {{신고수[1]}}</span>
+            <button @click="신고수[1]++">허위매물 신고</button>
+            <span>신고수 : {{신고수[1]}}</span>
         </div>
         <div>
+            <img src="./assets/room2.jpg" class="room-img">
             <h4>{{products[2]}}</h4>
             <p>70만원</p>
-            <button @click="신고수[2]++">허위매물 신고</button> <span>신고수 : {{신고수[2]}}</span>
+            <button @click="신고수[2]++">허위매물 신고</button>
+            <span>신고수 : {{신고수[2]}}</span>
         </div>
     </div>
 </template>
@@ -46,7 +59,8 @@
 export default {
     name: 'App',
     data(){
-        return {    
+        return {
+            modalOne : false,
             menus : ['home', 'Shop', 'About'],
             products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
             신고수 : [0,0,0]
@@ -69,7 +83,6 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
 }
 .menu{
     background-color: darkslateblue;
@@ -79,5 +92,29 @@ export default {
 .menu a{
     color: white;
     padding: 10px;
+}
+.room-img{
+    width : 100%;
+    margin-top : 40px;
+}
+body{
+    margin: 0;
+}
+div{
+    box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.white-bg h2{
+    border: 1px solid black;
+    padding: 15px;
 }
 </style>
