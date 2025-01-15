d2c6ec330368b3899dfc61b4ade69e16021364b1
DESKTOP-O9VECB5\admin <dud5825@naver.com>
2025-01-15 14:46:17 +0900

    [조영민] 2025-01-15 -14강 watcher로 데이터 감시 @input or v-mdoel 사용법 숙지 + 숙제 애매하게 완료

--- vuedongsan/src/components/Modal.vue
+++ vuedongsan/src/components/Modal.vue
@@ -8,10 +8,10 @@
             <!-- input에 사용 후 데이터값 저장하는데 사용.  -->
             <!-- input에 입력한 것은 전부 문자형 .number를 입력하면 숫자형으로 바뀌지만
               문자를 입력하는건 막을 수 없음 -->
-            <input v-model="month">
-            <select v-model="month">
+            <input v-model.number="month">
+            <!-- <select v-model.number="month">
                 <option v-for="selOpt in 12" :key="selOpt">{{selOpt}}</option>
-            </select>
+            </select> -->
             <p>{{month}} 개월 선택함 : 가격 : {{ month * oneRoom[modalIndex].price }} 원</p>
             <h2 @click="$emit('modalOff',false)">닫기</h2>
         </div>
@@ -22,14 +22,25 @@ export default {
     name : "ModalExport",
     data(){
         return{
-            month : 1,
+            month : "",
+            numberRegEx : /[0-9]/,
+        }
+    },
+    watch:{
+        // useEffect랑 비슷한듯?
+        // a는 변경후 b는 변경전 (매개변수)
+        month(a){
+            if(!this.numberRegEx.test(a)){
+                alert("테스트");
+                return this.month = 1;
+            }
         }
     },
     props :{
         oneRoom : Object,
         modalIndex : Number,
         modalOne : Boolean,
-    }
+    },
 }
 </script>
 <style lang="">
