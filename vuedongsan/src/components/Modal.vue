31ab9acb7e6d1425e844c03e8e271ea1ef798915
DESKTOP-O9VECB5\admin <dud5825@naver.com>
2025-01-15 11:10:01 +0900

    [조영민] 2025-01-15 -13강 @input / v-model 완료 숙제없음.

--- vuedongsan/src/components/Modal.vue
+++ vuedongsan/src/components/Modal.vue
@@ -4,7 +4,15 @@
             <img :src="oneRoom[modalIndex].image" class="room-img">
             <h4>{{ oneRoom[modalIndex].title }}</h4>
             <p>{{ oneRoom[modalIndex].content }}</p>
-            <p>가격 : {{ oneRoom[modalIndex].price }} 원</p>
+            <!-- <input @input="month = $event.target.value"> == v-model -->
+            <!-- input에 사용 후 데이터값 저장하는데 사용.  -->
+            <!-- input에 입력한 것은 전부 문자형 .number를 입력하면 숫자형으로 바뀌지만
+              문자를 입력하는건 막을 수 없음 -->
+            <input v-model="month">
+            <select v-model="month">
+                <option v-for="selOpt in 12" :key="selOpt">{{selOpt}}</option>
+            </select>
+            <p>{{month}} 개월 선택함 : 가격 : {{ month * oneRoom[modalIndex].price }} 원</p>
             <h2 @click="$emit('modalOff',false)">닫기</h2>
         </div>
     </div>
@@ -12,6 +20,11 @@
 <script>
 export default {
     name : "ModalExport",
+    data(){
+        return{
+            month : 1,
+        }
+    },
     props :{
         oneRoom : Object,
         modalIndex : Number,
