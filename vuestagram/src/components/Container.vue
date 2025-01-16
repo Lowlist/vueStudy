987d02a2c1c35e008ccb83dd04861123f836957a
DESKTOP-O9VECB5\admin <dud5825@naver.com>
2025-01-16 15:46:43 +0900

    [조영민] 2025-01-16 -3-1강 시작 vuestagram 프로젝트 생성 + 숙제 1까지 데이터 임포트 완료

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div>
+        <PostView></PostView>
+        <PostView></PostView>
+        <PostView></PostView>
+    </div>
+</template>
+<script>
+
+import postView from './Post.vue';
+
+export default {
+    name : "containerExport",
+    components: {
+        PostView: postView,
+    }
+}
+</script>
+<style>
+    
+</style>
